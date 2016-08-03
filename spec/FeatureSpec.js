11fec5912d8f6262d7c68744c035fedbfd258a57
Joe Sweeny <joesweeny84@hotmail.com>
2016-08-03 11:41:53 +0100

    User Story 2 - planes can take off

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Feature:', function(){
+  var plane;
+  var airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport)
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+});
--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -7,12 +7,18 @@ describe('Plane', function() {
 
   beforeEach(function() {
     plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['clearForLanding']);
+    airport = jasmine.createSpyObj('airport',['clearForLanding', 'clearForTakeOff']);
   });
 
   it('can land a plane at an airport', function() {
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+
+  it('can take off from an airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
   });
 
 });
